ecdeba6f033906d15c8e0af99fd03cd58f0431a1
YueKang17 <yue.kang1994@gmail.com>
2015-07-31 13:19:49 -0700

    Added new example project USART callback (transmits 8-bit string and reads from some other buffer). Updating USART unit test project after opening.

---
+++ SAMD21/Example Projects/USART_QUICK_START_CALLBACK1/USART_QUICK_START_CALLBACK1/USART_QUICK_START_CALLBACK1.c
@@ -0,0 +1,24 @@
+/*
+ * USART_QUICK_START_CALLBACK1.c
+ *
+ * Created: 7/31/2015 1:00:20 PM
+ *  Author: Cecily Jacquemin
+ */ 
+
+#include "sam.h"
+
+/**
+ * \brief Application entry point.
+ *
+ * \return Unused (ANSI-C compatibility).
+ */
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    while (1) 
+    {
+        //TODO:: Please write your application code 
+    }
+}
