73c34a3c84242911b6bf12e5d755754d6d5985fc
YueKang17 <yue.kang1994@gmail.com>
2015-07-31 15:11:41 -0700

    Added LED toggle project. Verified that MCU is recieving code properly and is connected to debug USB correctly.

---
+++ SAMD21/Example Projects/LED_TOGGLE1/LED_TOGGLE1/LED_TOGGLE1.c
@@ -0,0 +1,24 @@
+/*
+ * LED_TOGGLE1.c
+ *
+ * Created: 7/31/2015 3:05:49 PM
+ *  Author: Cecily Jacquemin
+ */ 
+
+#include "sam.h"
+
+/**
+ * \brief Application entry point.
+ *
+ * \return Unused (ANSI-C compatibility).
+ */
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    while (1) 
+    {
+        //TODO:: Please write your application code 
+    }
+}
